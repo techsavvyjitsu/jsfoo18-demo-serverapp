558421d44527a67716f45c6d50726e0a4033d9fc
ashegde <ashegde@paypal.com>
2018-06-11 00:40:47 +0530

    chore(prettier): config

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-	printWidth: 150,
-	tabWidth: 2,
-	useTabs: true,
-	semi: true,
-	singleQuote: true,
-	trailingComma: 'es5',
-	bracketSpacing: true,
-	arrowParens: 'always',
+  printWidth: 150,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
 };
